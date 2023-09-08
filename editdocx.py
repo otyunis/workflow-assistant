04f1332318616c8c824fbd2c214de16b786cb6b4
oty <otyunis@memphis.edu>
2023-09-08 08:51:56 -0500

    Modularize parsing w/ docx. Add editing functions.

--- editdocx.py
+++ editdocx.py
@@ -1,41 +1,57 @@
-from contextlib import redirect_stdout
 from docx import Document
+from docx.shared import Inches
+import copy
 
 class EditDocx:
     
     def __init__(self, file_path):
-        self.document = Document(file_path)
-        self.parsed_document_data = {}
-        self._parse_document()
-        
-    def _parse_document(self):
-        self.parsed_document_data['Document'] = {'Paragraphs':{}, 'Tables':{}}
-        self.parsed_document_data['Document']['Paragraphs'] = EditDocx._parse_paragraphs(self.document.paragraphs)
-        self.parsed_document_data['Document']['Tables'] = EditDocx._parse_tables(self.document.tables)
-        for s_num, section in enumerate(self.document.sections):
-            self.parsed_document_data[f'Section_{s_num}'] = {'Header':{'Paragraphs':{}, 'Tables':{}}, 'Footer':{'Paragraphs':{}, 'Tables':{}}}
-            
-            if section.header:
-                self.parsed_document_data[f'Section_{s_num}']['Header']['Paragraphs'] = EditDocx._parse_paragraphs(section.header.paragraphs)
-                self.parsed_document_data[f'Section_{s_num}']['Header']['Tables']= EditDocx._parse_tables(section.header.tables)
-            
-            if section.footer:
-                self.parsed_document_data[f'Section_{s_num}']['Footer']['Paragraphs'] = EditDocx._parse_paragraphs(section.footer.paragraphs)
-                self.parsed_document_data[f'Section_{s_num}']['Footer']['Tables']= EditDocx._parse_tables(section.footer.tables)
+        self.orig_document = Document(file_path)
+        self.edited_document = copy.deepcopy(self.orig_document)
+        self.parsed_orig_document = EditDocx._parse_document(self.orig_document)
+        self.parsed_edited_document = EditDocx._parse_document(self.edited_document)
+       
     
-    def print_parsed_document(self):
-        EditDocx._print_dict(self.parsed_document_data)
+    def print_parsed_document(self, orig=True):
+        if orig:
+            self.parsed_orig_document = EditDocx._parse_document(self.orig_document)
+            EditDocx._print_dict(self.parsed_orig_document)
+        else:
+            self.parsed_edited_document = EditDocx._parse_document(self.edited_document)
+            EditDocx._print_dict(self.parsed_edited_document)
     
     def insert_paragraph(): #will change # of paragraphs and thus affect subsequent document changes
         pass
     
-    def insert_image():
-        pass
+    def insert_image(self, img_path_or_stream, paragraph_num, width_in_inches): #TODO: allow for width and/or height or NONE
+        paragraph = self.edited_document.paragraphs[paragraph_num]
+        paragraph.add_run().add_picture(img_path_or_stream, width=Inches(width_in_inches))
     
-    def replace_paragraph_text():
-        
-    def replace_cell_text():
+    def replace_paragraph_text(self, paragraph_num, text):
         pass
+        
+    def replace_cell_text_in_table(self, table_num, row_num, col_num, text):
+        self.edited_document.tables[table_num].cell(row_num, col_num).text = text
+        
+    def save_edited_docx(self, save_path):
+        self.edited_document.save(save_path)
+    
+    @staticmethod
+    def _parse_document(docx_document):
+        parsed_document = {}
+        parsed_document['Document'] = {'Paragraphs':{}, 'Tables':{}}
+        parsed_document['Document']['Paragraphs'] = EditDocx._parse_paragraphs(docx_document.paragraphs)
+        parsed_document['Document']['Tables'] = EditDocx._parse_tables(docx_document.tables)
+        for s_num, section in enumerate(docx_document.sections):
+            parsed_document[f'Section_{s_num}'] = {'Header':{'Paragraphs':{}, 'Tables':{}}, 'Footer':{'Paragraphs':{}, 'Tables':{}}}
+            
+            if section.header:
+                parsed_document[f'Section_{s_num}']['Header']['Paragraphs'] = EditDocx._parse_paragraphs(section.header.paragraphs)
+                parsed_document[f'Section_{s_num}']['Header']['Tables']= EditDocx._parse_tables(section.header.tables)
+            
+            if section.footer:
+                parsed_document[f'Section_{s_num}']['Footer']['Paragraphs'] = EditDocx._parse_paragraphs(section.footer.paragraphs)
+                parsed_document[f'Section_{s_num}']['Footer']['Tables']= EditDocx._parse_tables(section.footer.tables)
+        return parsed_document
     
     @staticmethod
     def _parse_tables(docx_tables):
@@ -81,11 +97,3 @@ class EditDocx:
             else:
                 print(value)
                 
-    
-
-if __name__ == '__main__':
-    template_path = 'template.docx'
-    with open(f'{template_path.split(".")[0]}_parsed.txt', 'w') as f:
-        with redirect_stdout(f):
-            parsed_template = EditDocx(template_path)
-            parsed_template.print_parsed_document()
